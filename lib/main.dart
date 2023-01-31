6c8cf58e00416b5cb4f5724fb84416acee7d4256
ZiadAliAbdelaziz <ziadali0099@gmail.com>
2023-01-31 07:02:04 +0200

    first_commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:whatsapp_clone/features/landing/presentation/screens/landing_screen.dart';
+import 'package:whatsapp_clone/firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(411.42857142857144, 843.4285714285714),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (BuildContext context, Widget? child) {
+        return const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: LandingScreen(),
+        );
+      },
+    );
+  }
+}
